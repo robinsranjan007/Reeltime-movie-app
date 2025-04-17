e3462fabe8c63a1b6df2b1b019247b287ea2ab99
robinsranjan007 <robinsranjan619@gmail.com>
2025-04-17 03:30:07 -0400

    details ui ready

--- src/app/movies/moviesdetails/moviesdetails.component.ts
+++ src/app/movies/moviesdetails/moviesdetails.component.ts
@@ -3,11 +3,13 @@ import { ActivatedRoute } from '@angular/router';
 import { TmdbService } from '../../services/tmdb.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxStarsModule } from 'ngx-stars';
 
 @Component({
   selector: 'app-moviedetails',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule,MatIconModule,NgxStarsModule  ],
   templateUrl: './moviesdetails.component.html',
   styleUrls: ['./moviesdetails.component.css']
 })
@@ -26,4 +28,30 @@ export class MoviesdetailsComponent implements OnInit {
   getGenreNames(): string {
     return this.movie?.genres?.map((g: any) => g.name).join(', ') || '';
   }
+
+  selectedRating: number = 0;
+
+resetReview() {
+  this.selectedRating = 0;
+  const textarea = document.querySelector('textarea') as HTMLTextAreaElement;
+  if (textarea) textarea.value = '';
+}
+
+
+rating: number = 0;
+hovered: number = 0;
+
+setRating(value: number): void {
+  this.rating = value;
+}
+
+setHover(value: number): void {
+  this.hovered = value;
+}
+
+clearHover(): void {
+  this.hovered = 0;
+}
+
+
 }
